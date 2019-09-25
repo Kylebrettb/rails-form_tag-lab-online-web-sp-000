78b2f00566ece106d6ec9bcd43d282d92aec1b6b
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 19:24:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,5 @@
-d
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :new, :create]
+  get '/students/:id', to: 'students#show', as: 'student'
+end
